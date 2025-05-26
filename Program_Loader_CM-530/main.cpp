b62155c749f20cfc8a30c822be93f61dada58b52
D.Budnik <daniil.budnik@gmail.com>
2025-05-26 13:16:40 +0400

    Micro fix

--- Program_Loader_CM-530/main.cpp
+++ Program_Loader_CM-530/main.cpp
@@ -8,10 +8,25 @@
 #include "Logger.h"
 #include "ProgramLoader.h"
 
+const QString version = "1.1";
+
 int main(int argc, char* argv[]){
+
+    QApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
+    QApplication::setHighDpiScaleFactorRoundingPolicy(Qt::HighDpiScaleFactorRoundingPolicy::PassThrough);
+    QApplication::setAttribute(Qt::AA_ShareOpenGLContexts);
+
     QApplication app(argc, argv);
-    Log::Logger::initSoftware("[SSTU] Program Loader CM-530 [Author: Daniil Budnik]", "Version: 1.0 " +
-                              QDate::currentDate().toString("dd/MM/yyyy"));
+    Log::Logger::initSoftware("[SSTU] Program Loader CM-530 [Author: Daniil Budnik]", "Version: " +
+                              version + " " + QDate::currentDate().toString("dd/MM/yyyy"));
+
+    app.setAttribute(Qt::AA_UseHighDpiPixmaps);
+    app.setHighDpiScaleFactorRoundingPolicy(Qt::HighDpiScaleFactorRoundingPolicy::PassThrough);
+    app.setOrganizationName("SSTU");
+    app.setOrganizationDomain("https://github.com/Daniil-Budnik/Program-Loader-CM-530");
+    app.setApplicationName("Program Loader CM-530x");
+    app.setApplicationVersion(version);
+
     auto* loader = new ProgramLoader();
     loader->show();
     return QApplication::exec();
